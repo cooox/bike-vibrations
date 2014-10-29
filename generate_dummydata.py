d79c779c89dc3cc64509d5c7e5671f210dcae8c9
Dominik Pataky <git@netdecorator.org>
2014-10-29 16:49:07 +0100

    initial commit
    Signed-off-by: Dominik Pataky <git@netdecorator.org>

---
+++ generate_dummydata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+with open('dummydata_vibration.txt', 'w') as fh:
+    for i in range(5000):
+        data = { "lat": 51 + random.random(),
+                 "lon": 13 + random.random(),
+                 "height": 40 + random.random(),
+                 "datetime": 20141030143000,
+                 "easy": random.choice([0, 1]),
+                 "hard": random.choice([0, 1])
+        }
+        fh.write("%(lat)s,%(lon)s,%(height)s,%(datetime)s,%(easy)s,%(hard)s\n" % data)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Bike vibrations
+
+This is a draft.
+
+In the future, an Arduino will meter different sensors and this script shall put those values from a .txt into a database.
+
+Copyright 2014 Dominik Pataky
