6ffb8aee71cf9c1deeff3247216443e5e42e21cd
Dominik Pataky <git@netdecorator.org>
2014-11-10 17:52:06 +0100

    Fix and cleanup dummydata generator
    Signed-off-by: Dominik Pataky <git@netdecorator.org>

--- generate_dummydata.py
+++ generate_dummydata.py
@@ -2,13 +2,18 @@
 
 import random
 
+
+def r(basecoord):
+    # return random coordinates around a base coordinate
+    return basecoord + random.choice([random.random(), -random.random()]) * 0.001
+
 with open('dummydata_vibration.txt', 'w') as fh:
-    for i in range(5000):
-        data = { "lat": 51 + random.random(),
-                 "lon": 13 + random.random(),
+    for i in range(30):
+        data = { "lat": r(51.0294287),
+                 "lon": r(13.7235668),
                  "height": 40 + random.random(),
                  "timestamp": 20141030143000,
                  "easy": random.choice([0, 1]),
                  "hard": random.choice([0, 1])
         }
-        fh.write("%(lat)s,%(lon)s,%(height)s,%(datetime)s,%(easy)s,%(hard)s\n" % data)
+        fh.write("%(lat)s,%(lon)s,%(height)s,%(timestamp)s,%(easy)s,%(hard)s\n" % data)
