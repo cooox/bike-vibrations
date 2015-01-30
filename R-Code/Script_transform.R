dad122ac07da3b609b6b4cd339b216e963d2e6ea
gjorsch <g.braeunig@gmail.com>
2015-01-30 01:43:28 +0100

    R Export path to kml

--- R-Code/Script_transform.R
+++ R-Code/Script_transform.R
@@ -2,6 +2,7 @@
 library(RSQLite)
 library(DBI)
 library(sp)
+library(maptools)
 library(rgdal)
 
 setwd("D:/Users/Georg/Documents/R_workspace/Vibration/")
@@ -30,9 +31,26 @@ for(i in 1:x){
   temp_line <-Line(coords)
   pathes <- c(pathes, temp_line)
 }
-all_path = Lines(pathes,ID="a")
-all_path = SpatialLines(list(all_path))
+all_path = Lines(pathes,ID="path")
+all_path = SpatialLines(list(all_path), proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84"))
+path = SpatialLinesDataFrame(all_path, data.frame(Z = c("Road"), row.names = c("lines")), match.ID = FALSE)
 #all_path = SpatialLines(pathes,ID="a")
 
+#Create martix vibrationEvents
+vibration_st <- matrix(c(vibration_data$lon,vibration_data$lat,vibration_data$runtime,(sqrt(vibration_data$imu_x^2+vibration_data$imu_y^2+vibration_data$imu_z^2)),vibration_data$vibration_type),ncol=5)
+
+#Test strong Sensor
+sens_test <- vibration_st[which(vibration_st[,5]>=4), ]
+length(sens_test)
+
+#Filter: over 2G, under 5G
+vibration_st <- vibration_st[which(vibration_st[,4]>=4), ]
+vibration_st <- vibration_st[which(vibration_st[,4]<=25), ]
+#Eliminate duplicate Events
+x <- nrow(vibration_st)
+i=1
+for(i in 1:(x-1)){
+vibration_st <- vibration_st[which((vibration_st[i,3]-vibration_st[(i+1),3])>=100), ]
+}
 #Snap Vibration to path
-#snapPointsToLines(points, lines, maxDist = NA, withAttrs = TRUE, idField=NA)
+#snapPointsToLines(vibration_st, pathes, maxDist = NA, withAttrs = TRUE, idField=NA)
